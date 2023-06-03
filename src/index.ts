8b7837ef447b19844ab2b53b0a7f7f51a26f1270
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-03 13:45:32 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+    console.log("Server running on http://localhost:8080");
+});
